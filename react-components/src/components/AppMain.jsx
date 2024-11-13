aa82b3d59fe84b7908b07938772ddd710f8bd155
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-11-13 17:47:47 +0100

    pizza

--- react-components/src/components/AppMain.jsx
+++ react-components/src/components/AppMain.jsx
@@ -3,15 +3,15 @@ export default function AppMain() {
     const components = [
         {
             id: 1,
-            title: "Buona la pizza!",
-            description: "Lorem ipsum, dolor siat amet consectetur adipisicing elit. Omnis quisquam ut aperiam hic atque at ab rem corporis veniam repudiandae nihil, dolores ipsa laboriosam, recusandae soluta quo impedit. Unde, tempora.",
+            title: "Margherita",
+            description: "Lorem ipsum, Margherita siat amet consectetur adipisicing elit. Omnis quisquam ut aperiam hic atque at ab rem corporis veniam repudiandae nihil, dolores ipsa laboriosam, recusandae soluta quo impedit. Unde, tempora.",
             button: "LEGGI DI PIU'",
             img: "../assets/499725.jpg"
         },
         {
             id: 2,
-            title: "Buona la pizza!",
-            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis quisquam ut aperiam hic atque at ab rem corporis veniam repudiandae nihil, dolores ipsa laboriosam, recusandae soluta quo impedit. Unde, tempora.",
+            title: "Cacio e Pepe",
+            description: "Lorem ipsum, Cacio e Pepe sit amet consectetur adipisicing elit. Omnis quisquam ut aperiam hic atque at ab rem corporis veniam repudiandae nihil, dolores ipsa laboriosam, recusandae soluta quo impedit. Unde, tempora.",
             button: "LEGGI DI PIU'",
             img: "../assets/499725.jpg"
         }
@@ -24,11 +24,11 @@ export default function AppMain() {
             <section className="components">
                 {components.map(component =>
                     <div key={component.id}>
-                        <div>{components.img}</div>
+                        <img src="{component.img}"></img>
                         <div>
-                            <h3>{components.title}</h3>
-                            <div>{components.description}</div>
-                            <button>{components.button}</button>
+                            <h3>{component.title}</h3>
+                            <div>{component.description}</div>
+                            <button>{component.button}</button>
                         </div>
                     </div>)
                 }
