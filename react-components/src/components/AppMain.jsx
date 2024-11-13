493310dacf44c8773f856dda26a203fa671ecdcd
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-11-13 12:46:11 +0100

    working on cards

--- react-components/src/components/AppMain.jsx
+++ react-components/src/components/AppMain.jsx
@@ -1,18 +1,37 @@
-import img from '../../assets/499725.jpg';
-
 export default function AppMain() {
+
+    const components = [
+        {
+            id: 1,
+            title: "Buona la pizza!",
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis quisquam ut aperiam hic atque at ab rem corporis veniam repudiandae nihil, dolores ipsa laboriosam, recusandae soluta quo impedit. Unde, tempora.",
+            button: "LEGGI DI PIU'",
+            img: "../assets/499725.jpg"
+        },
+        {
+            id: 1,
+            title: "Buona la pizza!",
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis quisquam ut aperiam hic atque at ab rem corporis veniam repudiandae nihil, dolores ipsa laboriosam, recusandae soluta quo impedit. Unde, tempora.",
+            button: "LEGGI DI PIU'",
+            img: "../assets/499725.jpg"
+        }
+    ];
+
+    console.log(components);
 
     return (
         <main>
-            <section>
-                <div>
-                    <img src="{img}" alt="" />
-                    <div>
-                        <h3>Titolo del Post</h3>
-                        <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis quisquam ut aperiam hic atque at ab rem corporis veniam repudiandae nihil, dolores ipsa laboriosam, recusandae soluta quo impedit. Unde, tempora.</div>
-                        <button>LEGGI DI PIU'</button>
-                    </div>
-                </div>
+            <section className="components">
+                {components.map(component =>
+                    <div key={component.id}>
+                        <div>{components.img}</div>
+                        <div>
+                            <h3>{components.title}</h3>
+                            <div>{components.description}</div>
+                            <button>{components.button}</button>
+                        </div>
+                    </div>)
+                }
             </section>
         </main>
     )
